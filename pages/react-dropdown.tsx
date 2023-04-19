2f8875e5dbd0e3d8c31dd26c25c660702d49904e
Lee Delarm <lee.delarm@gmail.com>
2023-04-19 18:40:22 -0400

    Cleaning up code, adding styles, event listener

--- pages/react-dropdown.tsx
+++ pages/react-dropdown.tsx
@@ -1,51 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from '../styles/Dropdown.module.css'
 
+interface DropdownOption {
+    value: string,
+    label: string
+};
+
+const dungeonsAndDragonsClasses: DropdownOption[] = [
+    { value: '', label: 'Select a class' },
+    { value: 'barbarian', label: 'Barbarian' },
+    { value: 'bard', label: 'Bard' },
+    { value: 'cleric', label: 'Cleric' },
+    { value: 'druid', label: 'Druid' },
+    { value: 'fighter', label: 'Fighter' },
+    { value: 'monk', label: 'Monk' },
+    { value: 'paladin', label: 'Paladin' },
+    { value: 'ranger', label: 'Ranger' },
+    { value: 'rogue', label: 'Rogue' },
+    { value: 'sorcerer', label: 'Sorcerer' },
+    { value: 'warlock', label: 'Warlock' },
+    { value: 'wizard', label: 'Wizard' }
+]
+
 export default function ReactDropDown() {
-    const dungeonsAndDragonsClasses = [
-        { value: '', label: 'Select a class' },
-        { value: 'barbarian', label: 'Barbarian' },
-        { value: 'bard', label: 'Bard' },
-        { value: 'cleric', label: 'Cleric' },
-        { value: 'druid', label: 'Druid' },
-        { value: 'fighter', label: 'Fighter' },
-        { value: 'monk', label: 'Monk' },
-        { value: 'paladin', label: 'Paladin' },
-        { value: 'ranger', label: 'Ranger' },
-        { value: 'rogue', label: 'Rogue' },
-        { value: 'sorcerer', label: 'Sorcerer' },
-        { value: 'warlock', label: 'Warlock' },
-        { value: 'wizard', label: 'Wizard' }
-    ]
-
-    interface DropdownOption {
-        value: string,
-        label: string
-    };
-
     const [dropdownOptions, setDropdownOptions] = useState(dungeonsAndDragonsClasses);
     const [dropdownStatus, setDropdownStatus] = useState("closed");
     const [selectedOption, setSelectedOption] = useState<DropdownOption>(dropdownOptions[0]);
 
-    const dropdownToggle = (ref: any) => {
-        if (dropdownStatus == "closed") {
-            setDropdownStatus("open")
-        } else {
-            setDropdownStatus("closed")
+    const handleClickOutside = (event: any) => {
+        if (event.target.id != "slick-react-dropdown") {
+            setDropdownStatus("closed");
         }
+    };
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside, false);
+        return () => {
+            document.removeEventListener("click", handleClickOutside, false);
+        };
+    }, []);
+
+    const dropdownToggle = () => {
+        dropdownStatus == "closed" ? setDropdownStatus("open") : setDropdownStatus("closed");
+
     }
 
     const itemClick = (itemValue: string) => {
         const item: DropdownOption = dropdownOptions.find((option) => option.value == itemValue) || dropdownOptions[0];
         setSelectedOption(item);
+        setDropdownStatus("closed");
     };
-
 
     return (
         <>
             <div>Selected Option = {selectedOption.label} : {selectedOption.value}</div>
-            <div className={styles.dropdown}
-                onClick={dropdownToggle}>{dropdownStatus == "closed" && selectedOption.label}
+            <div className={styles.dropdown}>
+                {dropdownStatus == "closed" && <div className={styles.dropdownItem} id="slick-react-dropdown" onClick={dropdownToggle}>{selectedOption.label}</div>}
                 {dropdownStatus == "open" && dropdownOptions.map((option) => {
                     return (
                         <div key={option.value}
